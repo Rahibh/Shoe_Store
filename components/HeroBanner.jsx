aa2861c091d4fb6e222d232e846d6dfd5e608cd9
Rahib Haroon <Rahibharoon@gmail.com>
2025-01-08 01:12:47 +0500

    Add HeroBanner Slider

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -11,6 +11,26 @@ const HeroBanner = () => {
         showThumbs={false}
         showIndicators={false}
         showStatus={false}
+        renderArrowPrev={(clickHandler, hasPrev) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-[31px] md:right-[51px] bottom-0 w-[30px]
+          md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center
+          justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="text-sm md:text-lg text-white" />
+          </div>
+        )}
+        renderArrowNext={(clickHandler, hasPrev) => (
+          <div
+            onClick={clickHandler}
+            className="absolute right-0  bottom-0 w-[30px]
+          md:w-[50px] h-[30px] md:h-[50px] bg-black z-10 flex items-center
+          justify-center cursor-pointer hover:opacity-90"
+          >
+            <BiArrowBack className="rotate-180 text-sm md:text-lg  text-white" />
+          </div>
+        )}
       >
         <div>
           <img
